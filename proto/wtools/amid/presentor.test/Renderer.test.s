670bff5eb67f541f2118e89fdfd4a00363baaf42
dmvict <dm.vict.kr@gmail.com>
2021-08-19 09:41:32 +0300

    Add asset `basic` and test routine `trivial`

--- proto/wtools/amid/presentor.test/Renderer.test.s
+++ proto/wtools/amid/presentor.test/Renderer.test.s
@@ -7,10 +7,32 @@ if( typeof module !== 'undefined' )
 {
   const _ = require( '../../../node_modules/Tools' );
   _.include( 'wTesting' );
+  _.include( 'stxt' );
   require( '../presentor/Renderer.s' );
 }
 
 const _ = _global_.wTools;
+const __ = _globals_.testing.wTools;
+
+// --
+// context
+// --
+
+function onSuiteBegin()
+{
+  let context = this;
+  context.suiteTempPath = __.path.tempOpen( _.path.join( __dirname, '../..'  ), 'presentor' );
+  context.assetsOriginalPath = __.path.join( __dirname, '_assets' );
+}
+
+//
+
+function onSuiteEnd()
+{
+  let context = this;
+  _.assert( _.strHas( context.suiteTempPath, '/presentor' ) )
+  __.path.tempClose( context.suiteTempPath );
+}
 
 // --
 // tests
@@ -18,7 +40,16 @@ const _ = _global_.wTools;
 
 function trivial( test )
 {
+  const a = test.assetFor( 'basic' );
+  a.reflect();
+
+  var dataStr = a.fileProvider.fileRead( a.abs( 'Courses.stxt' ) );
+  var parser = _.stxt.Parser({ dataStr });
+  parser.form();
   debugger;
+
+  var node = parser.document.nodes[ 0 ].head;
+
   test.true( true );
 }
 
@@ -32,6 +63,16 @@ const Proto =
   name : 'Tools.ghi.Renderer',
   silencing : 1,
   enabled : 1,
+
+  onSuiteBegin,
+  onSuiteEnd,
+
+  context :
+  {
+    suiteTempPath : null,
+    assetsOriginalPath : null,
+    appJsPath : null,
+  },
 
   tests :
   {
